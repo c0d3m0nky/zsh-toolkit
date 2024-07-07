641e55f25fc55b4d6f473826d0e9f4d642b9a291
c0d3m0nk3y <4998029+c0d3m0nky@users.noreply.github.com>
2024-07-06 21:21:31 -0400

    Install script

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 export ZSHCOM__basedir=$(dirname "$0")
 self=$(basename "$0")
 
@@ -68,118 +70,6 @@ then
     echo No xml pretty printer
   fi
 fi
-
-if [[ -z $ZSHCOM_DSH_USEZSH ]]; then ZSHCOM_DSH_USEZSH=''; fi
-
-dockerShell() {
-	if [[ $ZSHCOM_DSH_USEZSH == *${1}* ]]; then
-		dockerZsh $1
-	else
-		docker exec -it $1 bash
-	fi
-}
-
-perfwatch() {
-	# shellcheck disable=SC2016
-	sensors | ack -i '^(CPU|MB) Temp:\s+([\+\-\d.]+°[CF])' --output '$1:\t$2'
-	echo ''
-	echo ''
-	# shellcheck disable=SC2016
-	(cat /proc/cpuinfo | ack "^([c]pu MHz|processor)\s+:\s(.+)$" --output '$2') | ack '(\d+) (\d)(\d+)\.\d+ (\d+) (\d)(\d+)\.\d+ ' --output '$1\t$2.$3  $5.$6\t$4'
-}
-
-dockerZsh() {
-	docker exec -it "$1" /bin/zsh
-}
-
-function pip-install-save { 
-    pip install "$1" && pip freeze | grep "$1" >> requirements.txt
-}
-
-# ToDo: check if top level is single dir
-un7zipall() {
-	for i in *.7z; do 7za x "$i" -o${i%%.7z}; done
-}
-
-unzipall() {
-	for i in *.zip; do unzip "$i" -d "${i%%.zip}"; done
-}
-
-unrarall() {
-	for i in *.rar; do unrar x "$i" "${i%%.rar}/"; done
-}
-
-function set_title() {
-  echo -e "\033]0;$1\007";
-}
-
-# ToDo: exclude ack's pid
-function psack () {
-  ps aux | ack -v '/usr/bin/ack' | ack -i $1
-}
-
-function fack () {
-  find ./ -type f | ack $1
-}
-
-function findWithExtension() {
-  badImg='\.(jpg_.+|r\d+|me-downloaders|torrent)$'
-
-  if [[ $1 == '-bimg' ]]
-  then
-    #echo "find ./ -type f | ack -i \"${badImg}\""
-    find ./ -type f | ack -i "${badImg}"
-    return
-  fi
-
-  if [[ $1 == '-empty' ]]
-   then
-     find ./ -type f | ack -i "/[^./]+$"
-     return
-   fi
-    
-
-  rx='\.('
-
-  # shellcheck disable=SC2068
-  for a in $@
-  do
-    if [[ $rx != '\.(' ]]
-    then
-      rx="${rx}|"
-    fi
-    rx="${rx}${a}"
-  done
-
-  rx="${rx})$"
-  #echo "find ./ -type f | ack \"${rx}\""
-  find ./ -type f | ack -i "${rx}"
-}
-
-function touchFiles() {
-  rec=''
-  root=$1
-
-  if [[ "$*" == *"-r"* ]]
-  then
-      rec='-r'
-  fi
-
-  if [[ ! -d $root ]]
-  then
-    echo invalid path
-    return
-  fi
-
-  if [[ $rec == '-r' ]]
-  then
-#    echo recurse
-    find "$root" -type f -exec touch {} +
-  else
-#    echo top
-    find "$root" -maxdepth 1  -type f -exec touch {} +
-  fi
-}
 
 if [[ $ZSHCOM_NOPY != true ]]
 then
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+if [[ -d $HOME/.zsh-toolkit ]]
+then
+  vared -p 'zsh-toolkit is already installed, would you like to reinstall? (y/n): ' -c resp
+
+  if [[ $resp != 'y' ]]; then
+    exit 0
+  fi
+
+  echo Deleting $HOME/.zsh-toolkit
+  rm -rf $HOME/.zsh-toolkit
+fi
+
+git clone git@github.com:c0d3m0nky/zsh-toolkit.git $HOME/.zsh-toolkit
+
+zshrcCheck=$(grep '^ZSHCOM=' $HOME/.zshrc)
+
+if [[ -z $zshrcCheck ]]
+then
+  cp $HOME/.zshrc $HOME/.zshrc.bak
+
+  echo '\n\n### zsh-toolkit init' >> $HOME/.zshrc
+  echo 'ZSHCOM="$HOME/.zsh-toolkit"' >> $HOME/.zshrc
+  echo 'source $ZSHCOM/init.sh' >> $HOME/.zshrc
+fi
+
+source $ZSHCOM/init.sh
