30d48e70e71aa864bf80a8c092887dd4fa7d162b
c0d3m0nk3y <4998029+c0d3m0nky@users.noreply.github.com>
2024-07-08 22:24:37 -0400

    update testing

--- py/pack/update.py
+++ py/pack/update.py
@@ -1,9 +1,31 @@
 import os
+import subprocess
 from pathlib import Path
 
 from git import Repo
 
 _basedir = Path(os.environ.get('ZSHCOM__basedir'))
 
+
+def _sh(cmd: str, check=False, suppress_error=False) -> str:
+    if suppress_error:
+        res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=check)
+    else:
+        res = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, check=check)
+
+    return res.stdout.decode('utf-8').strip()
+
+
+def is_behind_origin() -> bool:
+    resp = _sh('git status | grep -P "Your branch is behind .+ by (\d+) commit"')
+
+    return bool(resp)
+
 def main():
-    pass
+    repo = Repo(_basedir)
+    repo.remotes.origin.fetch()
+    diffs = repo.git.diff(f'origin/{repo.active_branch.name}')
+
+
+if __name__ == '__main__':
+    main()
