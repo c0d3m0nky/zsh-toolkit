9467fc9eb2cecb3181acc841ecd20b14dc7c92bd
c0d3m0nk3y <4998029+c0d3m0nky@users.noreply.github.com>
2024-08-08 17:57:04 -0400

    Missed setup import

--- py/pack/constants.py
+++ py/pack/constants.py
@@ -1,1 +1,1 @@
-zsh_toolkit_version = '1.8.12.12'
+zsh_toolkit_version = '1.8.12.13'
--- py/pack/setup.py
+++ py/pack/setup.py
@@ -7,7 +7,7 @@ setup(
     description='ZSH utils I find useful',
     author='c0d3m0nky',
     py_modules=[
-        'file_utils', 'utils', 'magic_files', 'logger',
+        'file_utils', 'utils', 'magic_files', 'logger', 'string_utils',
         'update', 'flatten', 'rxmv', 'folder_density', 'decomp', 'disk_usage', 'little_guys', 'dockur', 'replace_double_byte_chars'
     ],
     entry_points={
