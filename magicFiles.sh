f8bcee8cd5edbf5cdd1d7ad63ceb6931199a6ce7
c0d3m0nk3y <4998029+c0d3m0nky@users.noreply.github.com>
2024-11-19 23:12:21 -0500

    Cleanup magic files and sync logic with python

--- magicFiles.sh
+++ magicFiles.sh
@@ -3,9 +3,9 @@
 
 mf_transient=''
 
-if [[ -n "${ZSHCOM_transient}" &&  -d $ZSHCOM_transient ]]
+if [[ -n "${ZSHCOM_TRANSIENT}" &&  -d $ZSHCOM_TRANSIENT ]]
 then
-  mf_transient=$ZSHCOM_transient
+  mf_transient=$ZSHCOM_TRANSIENT
 elif [[ -n "${XDG_RUNTIME_DIR}" &&  -d $XDG_RUNTIME_DIR ]]
 then
   mf_transient=$XDG_RUNTIME_DIR/zsh_toolkit
@@ -16,38 +16,49 @@ then
   if [[ ${ZSHCOM__known_os:?} == 'win' ]]
   then
     mf_transient='/tmp/zsh_toolkit'
-    if [[ ! -d $mf_transient ]]
-    then
-      mkdir $mf_transient
-    fi
   elif [[ -d /dev/shm ]]
   then
     mf_transient='/dev/shm/zsh_toolkit'
-    if [[ ! -d $mf_transient ]]
-    then
-      mkdir $mf_transient
-    fi
   fi
 fi
 
+if [[ -n "${mf_transient}" && -d $mf_transient ]]
+then
+  export ZSHCOM__transient=$mf_transient
+else
+  echo "!!! Transient files not supported, set ZSHCOM_TRANSIENT in .zshrc"
+fi
 
-  if [[ -n "${mf_transient}" && -d $mf_transient ]]
+
+mkdir -p "$mf_transient"
+
+#region Cleaning old file locations
+
+clean_files=(
+  "${ZSHCOM__basedir:?}/.state_dependencies_checked"
+  "$ZSHCOM__basedir/.state_update_dependencies"
+  "$ZSHCOM__basedir/.state_trigger_resource"
+  "$ZSHCOM__basedir/.state_trigger_update"
+)
+
+for f in "${clean_files[@]}"
+do
+  if [ -f "${f}" ]
   then
-    export ZSHCOM__transient=$mf_transient
-  else
-    echo "!!! Transient cache not supported"
+    rm "$f"
   fi
+done
+
+#endregion
 
 # Shared with python
-mf_dependencies_checked="${ZSHCOM__basedir:?}/.state_dependencies_checked"
-mf_update_dependencies="$ZSHCOM__basedir/.state_update_dependencies"
-mf_trigger_resource="$ZSHCOM__basedir/.state_trigger_resource"
-mf_trigger_update="$ZSHCOM__basedir/.state_trigger_update"
+mf_dependencies_checked="$ZSHCOM__transient/.state_dependencies_checked"
+mf_update_dependencies="$ZSHCOM__transient/.state_update_dependencies"
+mf_trigger_resource="$ZSHCOM__transient/.state_trigger_resource"
+mf_trigger_update="$ZSHCOM__transient/.state_trigger_update"
 mf_repo_update_checked="$ZSHCOM__basedir/.state_repo_update_checked"
 mf_repo_updated="$ZSHCOM__basedir/.state_repo_updated"
 mf_init_data="$ZSHCOM__basedir/initData.json"
-
-
 
 mf_break_init="$ZSHCOM__basedir/.state_break_init"
 mf_init="$ZSHCOM__basedir/init.sh"
--- py/pack/magic_files.py
+++ py/pack/magic_files.py
@@ -1,20 +1,21 @@
 import os
 from pathlib import Path
-from typing import List, Union
+from typing import List
+from itertools import chain
 
 ztk_base_dir = Path(os.environ.get('ZSHCOM__basedir')).resolve()
 
-ztk_ram_cache: Union[Path, None] = os.environ.get('ZSHCOM__transient')
+_transient_env: str = os.environ.get('ZSHCOM__transient')
 
-if ztk_ram_cache:
-    ztk_ram_cache: Path = Path(os.environ.get('ZSHCOM__transient')).resolve()
-else:
-    ztk_ram_cache = None
+if not _transient_env:
+    raise Exception('!!! Transient files not supported, set ZSHCOM_TRANSIENT in .zshrc')
 
-dependencies_checked = ztk_base_dir / '.state_dependencies_checked'
-update_dependencies = ztk_base_dir / '.state_update_dependencies'
-trigger_re_source = ztk_base_dir / '.state_trigger_resource'
-trigger_update = ztk_base_dir / '.state_trigger_update'
+ztk_transient: Path = Path(_transient_env).resolve()
+
+dependencies_checked = ztk_transient / '.state_dependencies_checked'
+update_dependencies = ztk_transient / '.state_update_dependencies'
+trigger_re_source = ztk_transient / '.state_trigger_resource'
+trigger_update = ztk_transient / '.state_trigger_update'
 repo_update_checked = ztk_base_dir / '.state_repo_update_checked'
 repo_updated = ztk_base_dir / '.state_repo_updated'
 init_data = ztk_base_dir / 'initData.json'
@@ -24,6 +25,6 @@ _clear_cache_exclude: List[str] = ['.state_repo_update_checked']
 
 
 def clear_cache():
-    for f in ztk_base_dir.iterdir():
+    for f in chain(ztk_base_dir.iterdir(), ztk_transient.iterdir()):
         if f.is_file() and f.name not in _clear_cache_exclude and any([f.name.startswith(p) for p in _cache_prefixes]):
             f.unlink()
